169b8adf7cf4c48685b3ac1c9748d832b6e16cee
Joseph Notis <joseph.a.notis.21@dartmouth.edu>
2020-10-11 17:16:22 -0400

    counter resets and bugfixing

--- src/options.js
+++ src/options.js
@@ -2,6 +2,7 @@ updateBtn.onclick = () => {
   const newHost = document.getElementById('hostName');
   chrome.storage.sync.set({host: newHost.value}, () => {
     console.log(`Host updated to ${newHost.value}`);
+    chrome.storage.sync.set({count: 0}, () => { console.log('counter reset')});
     newHost.value = "";
   });
 };
--- src/popup.js
+++ src/popup.js
@@ -12,5 +12,5 @@ resetCounter.onclick = () => {
   counter.innerHTML = 0; // Visibly reset before syncing w/ storage
   chrome.storage.sync.set({count: 0}, () => {
     console.log("counter reset");
-  })
+  });
 };
